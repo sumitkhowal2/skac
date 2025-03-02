455976fd4b99fc294b66fbc525fab36c12557d08
sumit <sumit@kali>
2025-03-02 21:11:51 +0530

    2-march home update

--- script/script.js
+++ script/script.js
@@ -5,3 +5,29 @@ setTimeout(() => {
         alertBT.close();
     }
 }, 5000);
+
+const categoriesWrapper = document.querySelector('.categories-wrapper');
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+
+leftBtn.addEventListener('click', () => {
+    categoriesWrapper.scrollBy({ left: -200, behavior: 'smooth' });
+});
+
+rightBtn.addEventListener('click', () => {
+    categoriesWrapper.scrollBy({ left: 200, behavior: 'smooth' });
+});
+
+// Category selection functionality
+const categoryButtons = document.querySelectorAll('.category-btn');
+const products = document.querySelectorAll('.product');
+
+categoryButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        const category = button.getAttribute('data-category');
+        products.forEach(product => {
+            product.style.display = product.classList.contains(category) ? 'block' : 'none';
+        });
+    });
+});
+
